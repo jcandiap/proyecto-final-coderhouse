52733202159a0dbdd4dad5d7d877a37c469df546
Jose Candia <40003895+jcandiap@users.noreply.github.com>
2022-10-24 23:55:11 -0300

    Delete src/config directory

--- src/config/mongodb.js
+++
@@ -1,9 +0,0 @@
-export const optionsMongoDB = {
-    client: 'mongodb',
-    connection: {
-        user: 'sa',
-        password: 'mMr5McURj3FyWEiR',
-        database: 'ecommerce'
-    },
-    useNullAsDefault: true
-}
--- src/config/sqlite.js
+++
@@ -1,7 +0,0 @@
-export const optionsSqlite = {
-    client: 'sqlite3',
-    connection: {
-        filename: './src/db/sqlite/ecommerce.sqlite'
-    },
-    useNullAsDefault: true
-}
