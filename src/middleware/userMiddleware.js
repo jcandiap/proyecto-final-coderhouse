83012ef478e75e71e6974a7ed5ef2ad72f36daf0
Jose Candia <josecandiap@gmail.com>
2023-01-15 00:52:41 -0300

    feat: se agrega middleware valida datos registro

---
+++ src/middleware/userMiddleware.js
@@ -0,0 +1,32 @@
+export const userRegister = async (req, res, next) => {
+    const { email, password, nombre, direccion, edad, numeroTelefono, avatar } = req.body;
+    if( !Boolean(email) ) {
+        res.status(400).send({ error: 'Debe ingresar un email' });
+        return;
+    }
+    if( !Boolean(password) ) {
+        res.status(400).send({ error: 'Debe ingresar una contraseña' });
+        return;
+    }
+    if( !Boolean(nombre) ) {
+        res.status(400).send({ error: 'Debe ingresar un nombre' });
+        return;
+    }
+    if( !Boolean(direccion) ) {
+        res.status(400).send({ error: 'Debe ingresar una direccion' });
+        return;
+    }
+    if( !Boolean(edad) ) {
+        res.status(400).send({ error: 'Debe ingresar una edad' });
+        return;
+    }
+    if( !Boolean(numeroTelefono) ) {
+        res.status(400).send({ error: 'Debe ingresar un numero de telefono' });
+        return;
+    }
+    if( !Boolean(avatar) ) {
+        res.status(400).send({ error: 'Debe ingresar un avatar' });
+        return;
+    }
+    next();
+};
