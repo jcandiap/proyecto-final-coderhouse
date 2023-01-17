f23833cec926074cf5b68bf5ee4cb04f4ad2d155
Jose Candia <josecandiap@gmail.com>
2023-01-17 00:08:49 -0300

    feat: se agrega envio de sms

---
+++ src/notifications/sms.js
@@ -0,0 +1,19 @@
+import Twilio from 'twilio';
+
+export const sendMessageSms = async (telefono) => {
+    try {
+        const accountSid = process.env.TWILIO_SID;
+        const authToken = process.env.TWILIO_TOKEN;
+        const client = new Twilio(accountSid, authToken);
+        let message = 'Su compra ha sido recibida y ha sido confirmada con exito!\nGracias por comprar en Ecommerce Coderhouse!'
+        const options = {
+            body: message,
+            from: process.env.TWILIO_SMS_NUMBER,
+            to: telefono
+        }
+        const { status } = await client.messages.create(options);
+        return status;
+    } catch (error) {
+        console.error(error);
+    }
+}
