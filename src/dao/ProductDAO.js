deb8e92339a36c7f76fe55d1918915ca6913bc3a
Jose Candia <josecandiap@gmail.com>
2023-03-04 01:30:19 -0400

    feat: se agregan metodos de productos

--- src/dao/ProductDAO.js
+++ src/dao/ProductDAO.js
@@ -1,10 +1,11 @@
-import BaseDAO from "./config/BaseDAO";
+import { mongo } from "mongoose";
+import BaseDAO from "./config/BaseDAO.js";
 
 class ProductsDAO extends BaseDAO {
 
     constructor() {
         super();
-        this.collecton = 'products';
+        this.collection = 'products';
     }
 
     async save(newRegister) {
@@ -14,7 +15,28 @@ class ProductsDAO extends BaseDAO {
             await this.disconnect();
             return newRegister;
         } catch (error) {
-            throw new Error(`Error al guardar el registro [${ error?.message }]`);
+            throw new Error(`Error saving the product [${ error?.message }]`);
+        }
+    }
+
+    async get(object) {
+        try {
+            const collection = await this.connect();
+            const o_id = new mongo.ObjectId(object);
+            const product = await collection.findOne({ _id: o_id, status: 'active' });
+            return product;
+        } catch (error) {
+            throw new Error(`Error getting product [${ error?.message }]`)
+        }
+    }
+
+    async getAll() {
+        try {
+            const collection = await this.connect();
+            const products = await collection.find({}).toArray();
+            return products;
+        } catch (error) {
+            throw new Error(`Error getting products [${ error?.message }]`)
         }
     }
 
