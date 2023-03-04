6fc80df4fe7a57ef882a7ac810096a8fb6f53776
Jose Candia <josecandiap@gmail.com>
2023-03-04 01:10:24 -0400

    feat: se modifica os y logger de app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import productRoutes from './routes/products.js';
-import carRoutes from './routes/car.js';
 import userRouters from './routes/user.js';
 import dotenv from 'dotenv';
 import yargs from 'yargs/yargs';
 import cluster from 'cluster';
-import log4js from 'log4js';
+import { configureLogger, errorLogger, logger, warnLogger } from './config/logger.js';
+import os from 'os';
 
 dotenv.config();
+configureLogger();
 
 const argv = yargs(process.argv.slice(1))
     .default('modo', 'FORK')
@@ -15,22 +16,6 @@ const argv = yargs(process.argv.slice(1))
     .alias('c', 'cluster')
     .argv;
 
-log4js.configure({
-    appenders: {
-        console: { type: 'console' },
-        errors: { type: 'file', filename: 'error.log' },
-        warns: { type: 'file', filename: 'warn.log' }
-    },
-    categories: {
-        default: { appenders: ['console'], level: 'info' },
-        error: { appenders: ['errors', 'console'], level: 'error' },
-        warn: { appenders: ['warns', 'console'], level: 'warn' }
-    }
-});
-
-const logger = log4js.getLogger();
-const warnLogger = log4js.getLogger('warn');
-const errorLogger = log4js.getLogger('error');
 
 const SERVER_TYPE = argv.modo || 'FORK';
 
@@ -38,26 +23,24 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-let server = null;
-
 if( SERVER_TYPE !== 'FORK' ) {
     if( cluster.isPrimary ) {
-        for (let i = 0; i < os.cpus().length; i++) cluster.fork();
+        for (let i = 0; i < os.cpus().length; i++) {
+            cluster.fork()
+        };
     } else {
-        server = app.listen(PORT, () => logger.info(`Servidor iniciado en mondo ${ SERVER_TYPE } en el puerto ${ PORT } y el proceso ${ process.pid }`));
+        app.listen(PORT, () => logger.info(`Servidor iniciado en mondo ${ SERVER_TYPE } en el puerto ${ PORT } y el proceso ${ process.pid }`));
     }
 } else {
-    server = app.listen(PORT, () => logger.info(`Servidor iniciado en mondo ${ SERVER_TYPE } en el puerto ${ PORT } y el proceso ${ process.pid }`));
+    app.listen(PORT, () => logger.info(`Servidor iniciado en mondo ${ SERVER_TYPE } en el puerto ${ PORT } y el proceso ${ process.pid }`));
 }
 
-server.on('error', error => errorLogger.error(`Error en el servidor: ${ error }`));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/productos', productRoutes);
-app.use('/api/carrito', carRoutes);
-app.use('/api/usuario', userRouters);
+app.use('/api/product', productRoutes);
+app.use('/api/user', userRouters);
 
 app.use((req, res) => {
     warnLogger.warn(`Intentando ingresar una runa no implementada ${ req.baseUrl }${ req.url } método ${ req.method }`);
