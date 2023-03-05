1ff77101096c4fc95290b509adcb884583d2ac6a
Jose Candia <josecandiap@gmail.com>
2023-03-05 17:52:13 -0300

    feat: se agrega metodo login

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { errorLogger, logger } from '../config/logger.js';
 import UserDAO from '../dao/UserDAO.js';
 import { RegisterUserDTO, ReturnUserDTO } from '../dto/UserDTO.js';
+import bcrypt from 'bcrypt';
 
 const userContainer = new UserDAO();
 
@@ -23,7 +24,23 @@ export async function register(req, res) {
 }
 
 export async function login(req, res) {
-    logger.info('start method [save product]');
+    try {
+        logger.info('start method [save product]');
+        const { email, password } = req.body;
+        const userFound = await userContainer.getByEmail(email);
+        if( !userFound ) {
+            res.status(400).send({ status: 'error', message: 'User is not registered' });
+            return;
+        }
+        const passwordMatch = await bcrypt.compare(password, userFound.password);
+        if( passwordMatch ) {
+            res.status(200).send({ status: 'ok', message: 'User logged successfully', data: new ReturnUserDTO(userFound) })
+        } else {
+            res.status(400).send({ status: 'error', message: 'Loging failed' });
+        }
+    } catch (error) {
+        
+    }
 }
 
 export async function logout(req, res) {
