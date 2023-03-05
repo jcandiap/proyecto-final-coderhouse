54a02ff4ac2fb5df4ccd6f83a46359c00a0deb87
Jose Candia <josecandiap@gmail.com>
2023-03-05 17:04:31 -0300

    feat: se agrega register

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,31 @@
 import jwt from 'jsonwebtoken';
+import { errorLogger, logger } from '../config/logger.js';
+import UserDAO from '../dao/UserDAO.js';
+import { RegisterUserDTO, ReturnUserDTO } from '../dto/UserDTO.js';
+
+const userContainer = new UserDAO();
 
 export async function register(req, res) {
-    logger.info('start method [save product]');
-
+    try {
+        logger.info('start method [save product]');
+        const registerUser = new RegisterUserDTO(req.body);
+        const userFound = await userContainer.getByEmail(registerUser.email);
+        if( !!userFound ) {
+            res.status(400).send({ status: 'error', message: 'User is already registered' });
+            return;
+        }
+        await userContainer.save(registerUser);
+        res.status(200).send({ status: 'ok', message: 'User created', data: new ReturnUserDTO(registerUser) });
+    } catch ({ message }) {
+        errorLogger.error(message);
+        res.status(400).send({ status: 'error', message });
+    }
 }
 
 export async function login(req, res) {
     logger.info('start method [save product]');
+}
+
+export async function logout(req, res) {
+    logger.info('start method [logout]');
 }
--- src/dao/UserDAO.js
+++ src/dao/UserDAO.js
@@ -1,13 +1,35 @@
-import BaseDAO from "./config/BaseDAO";
+import BaseDAO from "./config/BaseDAO.js";
 
 class UserDAO extends BaseDAO {
 
-    async register(params) {
-        
+    constructor() {
+        super();
+        this.collection = 'users';
+    }
+
+    async save(object) {
+        try {
+            const collection = await this.connect();
+            await collection.insertOne(object);
+            await this.disconnect();
+        } catch ({ message }) {
+            throw new Error(`Error saving the user [${ message }]`)
+        }
     }
 
     async login(params) {
         
+    }
+
+    async getByEmail(email) {
+        try {
+            const collection = await this.connect();
+            const user = await collection.findOne({ email });
+            await this.disconnect();
+            return user;
+        } catch ({ message }) {
+            throw new Error(`Error getting user [${ message }]`);
+        }
     }
 
 }
