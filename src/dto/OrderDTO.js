c6f2c6562efb0baee0dfd0cf6ca0e0dbf1d8b64f
Jose Candia <josecandiap@gmail.com>
2023-03-14 15:38:30 -0300

    feat: se agrega dto para order

---
+++ src/dto/OrderDTO.js
@@ -0,0 +1,9 @@
+export class OrderDTO {
+    constructor(data) {
+        this.id = data?._id || '';
+        this.paymentId = data?.paymentId || '';
+        this.createdAt = data?.createdAt || 0;
+        this.total = data?.total || 0;
+        this.orderDetail = data?.orderDetail || [];
+    }
+}
