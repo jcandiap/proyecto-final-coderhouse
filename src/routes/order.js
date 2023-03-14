4e5e136b7b35e0d56eebbc6cc96c2e6d8a93f2f5
Jose Candia <josecandiap@gmail.com>
2023-03-14 16:09:54 -0300

    feat: se agrega metodo para confirmar orden

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { generateOrder, getOrderDetail } from '../controllers/OrderController.js';
+import { confirmOrder, generateOrder, getOrderDetail } from '../controllers/OrderController.js';
 import { validateGenerateOrder, validateGetOrder } from '../middleware/orderMiddleware.js';
 
 const orderRoutes = express.Router();
 
 orderRoutes.post('/generate-order', [validateGenerateOrder], generateOrder);
 orderRoutes.get('/get-order-detail/:id', [validateGetOrder], getOrderDetail);
-orderRoutes.post('/confirm-order');
+orderRoutes.post('/confirm-order', confirmOrder);
 
 export default orderRoutes;
