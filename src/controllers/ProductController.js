adedff2056740e84920451283a39219fc789ec75
Jose Candia <josecandiap@gmail.com>
2023-03-14 20:11:17 -0300

    feat: se agrega autorizacion para metodos de producto

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,12 +1,23 @@
+import jwt from 'jsonwebtoken';
+
 import { errorLogger, logger } from "../config/logger.js";
 import ProductsDAO from "../dao/ProductDAO.js";
+import UserDAO from "../dao/UserDAO.js";
 import { InsertProductDTO, ReturnInsertedProductDTO, ReturnProductDTO, UpdateProductDTO } from "../dto/ProductDTO.js";
 
 const productContainer = new ProductsDAO();
+const userContainer = new UserDAO();
 
 export async function saveProduct(req, res) {
     try {
         logger.info('start method [save product]');
+        const { authorization } = req.headers;
+        const { userId } = jwt.verify(authorization.split(' ')[2], process.env.SECRET);
+        const user = await userContainer.getById(userId);
+        if( !userId || !user.isAdmin ) {
+            res.status(400).send({ status: 'error', message: 'User no authorizated for this action' });
+            return;
+        }
         const insertProductDTO = new InsertProductDTO(req.body);
         const insertedProduct = await productContainer.save(insertProductDTO);
         const returnedInsertedProduct = new ReturnInsertedProductDTO(insertedProduct);
@@ -49,6 +60,13 @@ export async function getProducts(req, res) {
 export async function updateProduct(req, res) {
     try {
         logger.info('start method [update product]');
+        const { authorization } = req.headers;
+        const { userId } = jwt.verify(authorization.split(' ')[2], process.env.SECRET);
+        const user = await userContainer.getById(userId);
+        if( !userId || !user.isAdmin ) {
+            res.status(400).send({ status: 'error', message: 'User no authorizated for this action' });
+            return;
+        }
         const productFound = await productContainer.get(req.params.id);
         if( !productFound ) {
             res.status(400).send({ status: 'error', message: 'Product not found' });
@@ -66,6 +84,13 @@ export async function updateProduct(req, res) {
 export async function deleteProduct(req, res) {
     try {
         logger.info('start method [delete product]');
+        const { authorization } = req.headers;
+        const { userId } = jwt.verify(authorization.split(' ')[2], process.env.SECRET);
+        const user = await userContainer.getById(userId);
+        if( !userId || !user.isAdmin ) {
+            res.status(400).send({ status: 'error', message: 'User no authorizated for this action' });
+            return;
+        }
         const productFound = await productContainer.get(req.params.id);
         if( !productFound ) {
             res.status(400).send({ status: 'error', message: 'Product not found' });
