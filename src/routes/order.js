1f46c7a8a9f68a73719eba40f311c1970e367263
Jose Candia <josecandiap@gmail.com>
2023-03-14 12:26:50 -0300

    feat: se agrega metodo para generar orden

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { generateOrder } from '../controllers/OrderController.js';
 
 const orderRoutes = express.Router();
 
-orderRoutes.post('/generate-order');
+orderRoutes.post('/generate-order', generateOrder);
 orderRoutes.post('/get-order-detail');
 orderRoutes.post('/confirm-order');
 
